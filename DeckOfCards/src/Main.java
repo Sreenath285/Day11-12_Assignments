9a46cd26f4ac2896a1976aff9eaebed35ee730ca
Sreenath285 <70939307+Sreenath285@users.noreply.github.com>
2022-03-01 11:01:13 +0530

    Delete DeckOfCards directory

--- DeckOfCards/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        DeckOfCards deckOfCards = new DeckOfCards();
-        deckOfCards.shuffleCards();
-        String[] deck = deckOfCards.shuffleCards();
-        deckOfCards.displayCards(deck);
-    }
-}
